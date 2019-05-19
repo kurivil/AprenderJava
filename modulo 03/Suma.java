c1ffa4c7747fc0e3111ef69d8e33a9c2357be999
Rodrigo Bastias <rbastias@utem.cl>
2019-05-19 15:14:07 -0300

    Update "Suma and Practica"
    Although the program is correct, in the sections "//" mention is made of the entry of data, characters and invocations. It goes up in "practice" and in "suma.java".

--- modulo 03/Suma.java
+++ modulo 03/Suma.java
@@ -1,1 +1,32 @@
-unsupported reparse point
+//tipos de datos en java
+//  Primitivos (se escriben con minusculas)
+//      Enteros
+//          *byte (-128 hasta 127)
+//          *short(-32768 hasta 32767)        
+//          *int  (-2147483648 hasta 2147483647)
+//          *long (muy grande...)
+//      Decimales
+//          *float
+//          *double
+//      Otros
+//          *char   (un solo caracter)
+//          *boolean (true|false)
+//  Tipo objeto (hay que invocarlos, se escriben con mayusculas puesto que son clases)
+//          *String (cadenas de texto) :("Hola mundo")
+//  Variable
+//Espacio en memoria donde se van alojar informacion de tipo numerico o tipo texto
+//Consta de dos elementos:
+//Tipo:(datos, sea texto o numerico, seĂ±alado en los datos primitivos) 
+//Nombre: (nombre de la variable en cuestion, sin espacios y si se le quiere asignar espacio debe llevar un guion "_", o bien escribirlos semejante a esto "VariableCuantito" )
+//
+public class Suma {
+    public static void main (String args[]){
+
+        int num_1 = 9;
+        int num_2 = 14;
+        int resul = 0;
+
+        resul = num_1 + num_2;
+        System.out.println("el valor de tu suma es:" + resul);
+    }
+}
